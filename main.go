61290df7f8601c41e3679d7c31426d16b62332c4
Daniel Pavela <daniel.pavela@gmail.com>
2020-05-15 16:36:11 +1000

    everything is broken again

--- main.go
+++ main.go
@@ -7,21 +7,38 @@ import "os/exec"
 import "os"
 
 
-func print(text string){
+func print(text string) {
 	fmt.Println(text)
 }
+
+
+func help() {
+	fmt.Println("""
+	noti - a simple notification library written in go.
+	USAGE:
+	 noti [title] [message]
+	""")
+
+}
+
 
 func main(){
 	firstArg := os.Args[1]
 	secondArg := os.Args[2]
 
-	fmt.Println("Hello", runtime.GOOS)
-	if runtime.GOOS == "linux" {
-		print("Sending message...")
-		app := "notify-send"
-		command := exec.Command(app, firstArg, secondArg)
-		command.Output()
+	if len(os.Args) > 0 {
+		fmt.Println("Hello", runtime.GOOS)
 
+		if runtime.GOOS == "linux" {
+			print("Sending message...")
+			app := "notify-send"
+			command := exec.Command(app, firstArg, secondArg)
+			command.Output()
+
+		}
+
+	}	else {
+		help()
 
 	}
 
